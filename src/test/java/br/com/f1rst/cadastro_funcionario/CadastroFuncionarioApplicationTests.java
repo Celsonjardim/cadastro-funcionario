c3480b5f89c079975d53e15749ec7ace158b4704
Celson Jardim <celsonjardim.dev@outlook.com>
2025-10-17 20:50:56 -0300

    inicia o projeto

---
+++ src/test/java/br/com/f1rst/cadastro_funcionario/CadastroFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.f1rst.cadastro_funcionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroFuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
