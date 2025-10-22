63b047e34790a78953698130262f8f5fe6478e71
Celson Jardim <celsonjardim.dev@outlook.com>
2025-10-21 21:28:16 -0300

    implementa request e response

--- src/main/java/br/com/f1rst/cadastro_funcionario/Funcionario/application/api/FuncionarioRequest.java
+++ src/main/java/br/com/f1rst/cadastro_funcionario/Funcionario/application/api/FuncionarioRequest.java
@@ -1,4 +1,18 @@
 package br.com.f1rst.cadastro_funcionario.Funcionario.application.api;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
 public class FuncionarioRequest {
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    private String designacao;
+    @NotBlank
+    private String salario;
+    @NotBlank
+    private String celular;
+    @NotBlank
+    private String endereco;
 }
--- src/main/java/br/com/f1rst/cadastro_funcionario/Funcionario/application/api/FuncionarioResponse.java
+++ src/main/java/br/com/f1rst/cadastro_funcionario/Funcionario/application/api/FuncionarioResponse.java
@@ -1,4 +1,10 @@
 package br.com.f1rst.cadastro_funcionario.Funcionario.application.api;
 
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
 public class FuncionarioResponse {
+    private UUID idFuncionario;
 }
